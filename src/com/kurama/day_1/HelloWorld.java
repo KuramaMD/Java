9984d1c9c37d2ed1473449e2e66924005a07a604
KuramaMD <cicati9@gmail.com>
2021-09-06 15:52:23 +0300

    Commit test!

---
+++ src/com/kurama/day_1/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.kurama.day_1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // A java comment
+    }
+
+}
