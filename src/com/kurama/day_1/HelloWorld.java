8062fb6b26fe3d17f0790b0649c3c63b31ee7841
KuramaMD <cicati9@gmail.com>
2021-09-07 14:33:04 +0300

    Update

--- src/com/kurama/day_1/HelloWorld.java
+++ src/com/kurama/day_1/HelloWorld.java
@@ -2,13 +2,24 @@ package com.kurama.day_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        double gallons, liters;
+        int a, b, c;
 
-        gallons = 1;
+        a = 2;
+        b = 3;
 
-        liters = gallons * 3.7854;
+        if(a < 3) System.out.println("a is less than b");
+        if(a == b) System.out.println("This won't display");
+        System.out.println();
 
-        System.out.println(gallons + " gallons is " + liters + " liters");
+        c = a - b;
+
+        if(c >= 0) System.out.println("C is not negative " + c);
+        if(c < 0) System.out.println("C is negative " + c);
+        System.out.println();
+
+        c = b - a;
+        if(c >= 0) System.out.println("C is not negative " + c);
+        if(c < 0) System.out.println("C is negative " + c);
 
         // A java comment
     }
