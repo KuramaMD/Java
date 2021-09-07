d298dca0de49fdcaa02b16350436e59caba8e15d
KuramaMD <cicati9@gmail.com>
2021-09-07 14:18:02 +0300

    Update

--- src/com/kurama/day_1/HelloWorld.java
+++ src/com/kurama/day_1/HelloWorld.java
@@ -2,7 +2,21 @@ package com.kurama.day_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int myVar1, myVar2;
+        double rand, myVar3;
+
+        myVar1 = 1024;
+        System.out.println("my Var = " + myVar1);
+
+        myVar2 = myVar1 / 2;
+        System.out.println("My var 2 = " + myVar2);
+
+        myVar3 = 10.33;
+
+        rand = myVar3 / 4;
+        myVar3 = myVar3/ 4;
+        System.out.println(rand);
+        System.out.println(myVar3);
         // A java comment
     }
 
